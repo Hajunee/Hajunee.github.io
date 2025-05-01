b43e7787c34d498a3e16894e148fd1b0470d0cd2
Hajunee <ljkbarav@gmail.com>
2025-05-01 08:57:17 +0000

    Deploying to gh-pages from @ Hajunee/Hajunee.github.io@f1158ed45af476f511622ad9f659713bb7a94b99 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -80,7 +80,7 @@ ninja.data = [{
         section: "Posts",
         handler: () => {
           
-            window.location.href = "/blog/2025/Markdown-blog-post/";
+            window.location.href = "/blog/2025/markdown/";
           
         },
       },{id: "post-나의-첫-jekyll-블로그-포스트",
